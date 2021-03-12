608a8aa87c2e81f5b1e87c1dfd7b3205e21c4ca9
Chady Ghribi <Chady>
2021-03-12 11:51:31 +0100

    second commit

---
+++ client/src/component/Subscribers.js
@@ -0,0 +1,28 @@
+import React,{useState} from 'react'
+import axios from 'axios'
+
+const Subscribers = () => {
+    const [sub, setSub] = useState([])
+    const [fullName, setFullName] = useState('')
+    const [email, setEmail] = useState('')
+
+    axios.get('/api/newsletter')
+    .then ((res)=> { setSub(res.data.subscribers)})
+    .catch ((err)=>console.log(err))
+
+    const handleAdd =()=> {
+        axios.post('/api/newsletter',{fullName,email})
+        .then ((res)=>console.log(res.data.msg))
+        .catch ((err)=>console.log(err))    
+    }
+    return (
+        <div>
+            <input onChange={e=>setFullName(e.target.value)}></input>
+            <input onChange={e=>setEmail(e.target.value)}></input>
+            <button onClick={handleAdd}>add</button>
+            {sub.map(el=> <h1>{el.fullName}</h1>)}
+        </div>
+    )
+}
+
+export default Subscribers
