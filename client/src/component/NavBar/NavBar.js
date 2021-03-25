757b7c6102748294523f92d79d349ee658633e9a
Slim Masmoudi <slimmasmoudi@ymail.com>
2021-03-25 18:01:00 +0100

    responsive navbar

--- client/src/component/NavBar/NavBar.js
+++ client/src/component/NavBar/NavBar.js
@@ -8,23 +8,28 @@ const NavBar = () => {
       <div className="navigation-logo">
         <img width="180px" src="./logo.png" alt="logo" />
       </div>
-      <ul>
-        <li>
-          <a href="/">Accueil</a>
-        </li>
-        <li>
-          <a href="/">A propos de nous</a>
-        </li>
-        <li>
-          <a href="/">Nos réalisations</a>
-        </li>
-        <li>
-          <a href="/">Blog</a>
-        </li>
-        <li>
-          <a href="/">Contactez-nous</a>
-        </li>
-      </ul>
+      <div className="navigation-ul">
+        <ul>
+          <li>
+            <a href="/">Accueil</a>
+          </li>
+          <li>
+            <a href="/">A propos de nous</a>
+          </li>
+          <li>
+            <a href="/">Nos réalisations</a>
+          </li>
+          <li>
+            <a href="/">Blog</a>
+          </li>
+          <li>
+            <a href="/">Contactez-nous</a>
+          </li>
+        </ul>
+        <div className="menu-icon">
+          <MenuIcon fontSize="large" />
+        </div>
+      </div>
     </div>
   );
 };
