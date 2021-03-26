ed6b164b59216275517f3542ba9acd1a240fa124
Slim Masmoudi <slimmasmoudi@ymail.com>
2021-03-26 13:28:39 +0100

    react scroll

--- client/src/component/NavBar/NavBar.js
+++ client/src/component/NavBar/NavBar.js
@@ -1,17 +1,27 @@
 import React from "react";
 import MenuIcon from "@material-ui/icons/Menu";
+import Scroll from "react-scroll";
 import "./NavBar.css";
 
 const NavBar = () => {
+  let scroll = Scroll.animateScroll;
+  const scrollTop = (e) => {
+    scroll.scrollToTop();
+    e.preventDefault();
+  };
   return (
     <div className="navigation-menu">
       <div className="navigation-logo">
-        <img width="180px" src="./logo.png" alt="logo" />
+        <a href="/">
+          <img width="180px" src="./logo.png" alt="logo" />
+        </a>
       </div>
       <div className="navigation-ul">
         <ul>
           <li>
-            <a href="/">Accueil</a>
+            <a href="#" onClick={scrollTop}>
+              Accueil
+            </a>
           </li>
           <li>
             <a href="/">A propos de nous</a>
